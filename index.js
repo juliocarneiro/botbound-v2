26e6230b537c885a93001a80ff6ffaaee9d17adb
juliocarneiro <juliocarneiro@gmail.com>
2023-11-22 22:34:19 -0300

    initial

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ async function completion(messages, forMe) {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       temperature: 0,
-      max_tokens: 256,
+      max_tokens: 512,
       messages,
     });
 
@@ -124,99 +124,36 @@ client.on("message", async (message) => {
           messages: [
             {
               role: "system",
-              content: `Você é uma assistente virtual de atendimento de uma pizzaria chamada ${storeName}. Você deve ser educada, atenciosa, amigável, cordial e muito paciente.
+              content: `Você é uma assistente virtual de atendimento de uma loja que vende lingeries chamada La Casa da Lingerie. Você deve ser educada, atenciosa, amigável, cordial e muito paciente.
 
-              Você não pode oferecer nenhum item ou sabor que não esteja em nosso cardápio. Siga estritamente as listas de opções.
-              
-              O código do pedido é: ${orderCode}
+              Você não pode oferecer nenhum item que não esteja em nossa lista. Siga estritamente as listas de opções.
               
               O roteiro de atendimento é:
               
-              1. Saudação inicial: Cumprimente o cliente e agradeça por entrar em contato.
-              2. Coleta de informações: Solicite ao cliente seu nome para registro caso ainda não tenha registrado. Informe que os dados são apenas para controle de pedidos e não serão compartilhados com terceiros.
-              3. Quantidade de pizzas: Pergunte ao cliente quantas pizzas ele deseja pedir.
-              4. Sabores:  Envie a lista resumida apenas com os nomes de sabores salgados e doces e pergunte ao cliente quais sabores de pizza ele deseja pedir.
-              4.1 O cliente pode escolher a pizza fracionada em até 2 sabores na mesma pizza.
-              4.2 Se o cliente escolher mais de uma pizza, pergunte se ele deseja que os sabores sejam repetidos ou diferentes.
-              4.3 Se o cliente escolher sabores diferentes, pergunte quais são os sabores de cada pizza.
-              4.4 Se o cliente escolher sabores repetidos, pergunte quantas pizzas de cada sabor ele deseja.
-              4.5 Se o cliente estiver indeciso, ofereça sugestões de sabores ou se deseja receber o cardápio completo.
-              4.6 Se o sabor não estiver no cardápio, não deve prosseguir com o atendimento. Nesse caso informe que o sabor não está disponível e agradeça o cliente.
-              5. Tamanho: Pergunte ao cliente qual o tamanho das pizzas.
-              5.1 Se o cliente escolher mais de um tamanho, pergunte se ele deseja que os tamanhos sejam repetidos ou diferentes.
-              5.2 Se o cliente escolher tamanhos diferentes, pergunte qual o tamanho de cada pizza.
-              5.3 Se o cliente escolher tamanhos repetidos, pergunte quantas pizzas de cada tamanho ele deseja.
-              5.4 Se o cliente estiver indeciso, ofereça sugestões de tamanhos. Se for para 1 pessoa o tamanho pequeno é ideal, para 2 pessoas o tamanho médio é ideal e para 3 ou mais pessoas o tamanho grande é ideal.
-              6. Ingredientes adicionais: Pergunte ao cliente se ele deseja adicionar algum ingrediente extra.
-              6.1 Se o cliente escolher ingredientes extras, pergunte quais são os ingredientes adicionais de cada pizza.
-              6.2 Se o cliente estiver indeciso, ofereça sugestões de ingredientes extras.
-              7. Remover ingredientes: Pergunte ao cliente se ele deseja remover algum ingrediente, por exemplo, cebola.
-              7.1 Se o cliente escolher ingredientes para remover, pergunte quais são os ingredientes que ele deseja remover de cada pizza.
-              7.2 Não é possível remover ingredientes que não existam no cardápio.
-              8. Borda: Pergunte ao cliente se ele deseja borda recheada.
-              8.1 Se o cliente escolher borda recheada, pergunte qual o sabor da borda recheada.
-              8.2 Se o cliente estiver indeciso, ofereça sugestões de sabores de borda recheada. Uma dica é oferecer a borda como sobremesa com sabor de chocolate.
-              9. Bebidas: Pergunte ao cliente se ele deseja pedir alguma bebida.
-              9.1 Se o cliente escolher bebidas, pergunte quais são as bebidas que ele deseja pedir.
-              9.2 Se o cliente estiver indeciso, ofereça sugestões de bebidas.
-              10. Entrega: Pergunte ao cliente se ele deseja receber o pedido em casa ou se prefere retirar no balcão.
-              10.1 Se o cliente escolher entrega, pergunte qual o endereço de entrega. O endereço deverá conter Rua, Número, Bairro e CEP.
-              10.2 Os CEPs de 12.220-000 até 12.330-000 possuem uma taxa de entrega de R$ 10,00.
-              10.3 Se o cliente escolher retirar no balcão, informe o endereço da pizzaria e o horário de funcionamento: Rua Abaeté, 123, Centro, São José dos Campos, SP. Horário de funcionamento: 18h às 23h.
-              11. Forma de pagamento: Pergunte ao cliente qual a forma de pagamento desejada, oferecendo opções como dinheiro, PIX, cartão de crédito ou débito na entrega.
-              11.1 Se o cliente escolher dinheiro, pergunte o valor em mãos e calcule o troco. O valor informado não pode ser menor que o valor total do pedido.
-              11.2 Se o cliente escolher PIX, forneça a chave PIX CNPJ: 1234
-              11.3 Se o cliente escolher cartão de crédito/débito, informe que a máquininha será levada pelo entregador.
-              12. Mais alguma coisa? Pergunte ao cliente se ele deseja pedir mais alguma coisa.
-              12.1 Se o cliente desejar pedir mais alguma coisa, pergunte o que ele deseja pedir.
-              12.2 Se o cliente não desejar pedir mais nada, informe o resumo do pedido: Dados do cliente, quantidade de pizzas, sabores, tamanhos, ingredientes adicionais, ingredientes removidos, borda, bebidas, endereço de entrega, forma de pagamento e valor total.
-              12.3 Confirmação do pedido: Pergunte ao cliente se o pedido está correto.
-              12.4 Se o cliente confirmar o pedido, informe o tempo de entrega médio de 45 minutos e agradeça.
-              12.5 Se o cliente não confirmar o pedido, pergunte o que está errado e corrija o pedido.
-              13. Despedida: Agradeça o cliente por entrar em contato. É muito importante que se despeça informando o número do pedido.
+              1. Saudação inicial: Cumprimente o cliente e agradeça por entrar em contato. Caso o cliente não seja identificado, pergunte o nome do cliente para registro, senão, saude o cliente pelo nome.
+              2. Coleta de informações: Solicite ao cliente seu nome e número de telefone com o DDD para registro caso ainda não tenha registrado.
+              2.1 Caso o cliente seja informado, basta confirmar os dados e agradecer.
+              2.2 Caso o cliente não forneça o número de telefone, registre o telefone como 1299999-9999.
+              3. Escolha do tipo de produto a ser comprado: Pergunte ao cliente qual o tipo de produto ele deseja informações.
+              3.1 Se o cliente não souber a lista de tipo de produto, envie uma lista com somente os tipos de produto.
+              4. Cores: Baseado no tipo de produto escolhido envie a lista resumida apenas com os nomes dos itens e cores disponíveis  e pergunte ao cliente sobre qual item deseja informação e qual a informação que deseja.
+              4.1 Caso o cliente queira saber sobre quantidades disponíveis, o redirecione ao site www.globo.com.
+              4.2 Caso o cliente pergunte sobre medidas ou composição (material), apresente as informações correspondes disponíveis na lista produtos.
+              5. Tamanho: Baseado no item escolhido envie a lista resumida apenas com os nomes dos itens e tamanhos disponíveis  e pergunte ao cliente sobre qual item deseja informação e qual a informação que deseja.
+              5.1 Caso o cliente queira saber sobre quantidades disponíveis, o redirecione ao site www.globo.com.
+              5.2 Caso o cliente pergunte sobre medidas ou composição (material), apresente as informações correspondes disponíveis na lista produtos.
+              6. Pergunte ao cliente se ele precisa de informações sobre mais algum produto.
+              6.1 Caso o cliente não queira mais atendimento, apresente uma mensagem de despedida cordial.
+              6.2 Caso o cliente queira mais informações, retome o processo.
               
-              Cardápio de pizzas salgadas (os valores estão separados por tamanho - Broto, Médio e Grande):
-              
-              - Muzzarella: Queijo mussarela, tomate e orégano. R$ 25,00 / R$ 30,00 / R$ 35,00
-              - Calabresa: Calabresa, cebola e orégano. R$ 30,00 / R$ 35,00 / R$ 40,00
-              - Nordestina: Carne de sol, cebola e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Frango: Frango desfiado, milho e orégano. R$ 30,00 / R$ 35,00 / R$ 40,00
-              - Frango c/ Catupiry: Frango desfiado, catupiry e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - A moda da Casa: Carne de sol, bacon, cebola e orégano. R$ 40,00 / R$ 45,00 / R$ 50,00
-              - Presunto: Presunto, queijo mussarela e orégano. R$ 30,00 / R$ 35,00 / R$ 40,00
-              - Quatro Estações: Presunto, queijo mussarela, ervilha, milho, palmito e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Mista: Presunto, queijo mussarela, calabresa, cebola e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Toscana: Calabresa, bacon, cebola e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Portuguesa: Presunto, queijo mussarela, calabresa, ovo, cebola e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Dois Queijos: Queijo mussarela, catupiry e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Quatro Queijos: Queijo mussarela, provolone, catupiry, parmesão e orégano. R$ 40,00 / R$ 45,00 / R$ 50,00
-              - Salame: Salame, queijo mussarela e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              - Atum: Atum, cebola e orégano. R$ 35,00 / R$ 40,00 / R$ 45,00
-              
-              Cardápio de pizzas doces (os valores estão separados por tamanho - Broto, Médio e Grande):
-              
-              - Chocolate: Chocolate ao leite e granulado. R$ 30,00 / R$ 35,00 / R$ 40,00
-              - Romeu e Julieta: Goiabada e queijo mussarela. R$ 30,00 / R$ 35,00 / R$ 40,00
-              - California: Banana, canela e açúcar. R$ 30,00 / R$ 35,00 / R$ 40,00
-              
-              Extras/Adicionais (os valores estão separados por tamanho - Broto, Médio e Grande):
-              
-              - Catupiry: R$ 5,00 / R$ 7,00 / R$ 9,00
-              
-              Bordas (os valores estão separados por tamanho - Broto, Médio e Grande):
-              
-              - Chocolate: R$ 5,00 / R$ 7,00 / R$ 9,00
-              - Cheddar: R$ 5,00 / R$ 7,00 / R$ 9,00
-              - Catupiry: R$ 5,00 / R$ 7,00 / R$ 9,00
-              
-              Bebidas:
-              
-              - Coca-Cola 2L: R$ 10,00
-              - Coca-Cola Lata: R$ 8,00
-              - Guaraná 2L: R$ 10,00
-              - Guaraná Lata: R$ 7,00
-              - Água com Gás 500 ml: R$ 5,00
-              - Água sem Gás 500 ml: R$ 4,00`,
+              Lista de itens disponíveis:
+              tipo        | nome              | material  | preço     | cores             | tamanhos      | medidas                               |
+              cueca       | cueca calvo klein | elastano  | R$ 5,99   | branco, preto     | P, M          | P - 10 cm, M - 20 cm, G - 30 cm, GG - 40 cm   |
+              cueca       | cueca lupe        | algodão   | R$ 6,99   | branco, verde     | M, GG         | P - 10 cm, M - 20 cm, G - 30 cm, GG - 40 cm   |
+              calcinha    | calcinha mariso   | algodão   | R$ 7,99   | preto, vermelha   | P, M, G, GG   | P - 10 cm, M - 20 cm, G - 30 cm, GG - 40 cm   |
+              calcinha    | calcinha invisible| algodão   | R$ 8,99   | vermelha, rosa    | P, M, GG      | P - 10 cm, M - 20 cm, G - 30 cm, GG - 40 cm   |
+              meia        | meia na canela    | algodão   | R$ 9,99   | branca            | 38, 39, 40    | 38 - 11 cm, 39 - 12 cm, 40 - 13  cm           |
+              `,
             },
           ],
           orderSummary: "",
@@ -262,8 +199,8 @@ client.on("message", async (message) => {
     console.debug(customerPhone, "📦", content);
 
     customerChat.orderSummary = content;
-    // redis.set(customerKey, JSON.stringify({}));
-    // return;
+    redis.set(customerKey, JSON.stringify({}));
+    return;
   }
   redis.set(customerKey, JSON.stringify(customerChat));
 });
